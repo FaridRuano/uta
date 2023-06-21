46b720716fbe778b28bc93565954a49b65c603d4
Diego Cata <difercata26@gmail.com>
2023-06-20 22:25:16 -0500

    sidebar corregido

--- src/components/common/sidebar/sidebar.jsx
+++ src/components/common/sidebar/sidebar.jsx
@@ -1,15 +1,15 @@
 import React, { Fragment, useEffect, useState } from "react"
 import { MENUITEMS } from './menu'
 import { Link } from "react-router-dom"
-import logo from "../../../assets/images/logo-uta.png"
+import  logo from "../../../assets/images/logo-uta.png"
 import { LogOut, Box } from "react-feather";
 import "./style.scss"
 
 const Sidebar = ({ isOpen }) => {
-	const [mainmenu, setMainMenu] = useState(MENUITEMS)
+  const [mainmenu, setMainMenu] = useState(MENUITEMS)
 	const [isChange, setIsChange] = useState(false)
 
-	useEffect(() => {
+  useEffect(() => {
 		const currentUrl = window.location.pathname;
 		mainmenu.map((items) => {
 			mainMenu.filter((Items) => {
@@ -19,14 +19,14 @@ const Sidebar = ({ isOpen }) => {
 					if (subItems.path === currentUrl) setNavActive(subItems);
 					if (!subItems.children) return false;
 					subItems.children.filter((subSubItems) => {
+						if (subSubItems.path === currentUrl) {
+							setNavActive(subSubItems);
+							return true;
+						} else {
+							return false;
+						}
 					});
 					return subItems;
-					if (subSubItems.path === currentUrl) {
-						setNavActive(subSubItems);
-						return true;
-					} else {
-						return false;
-					}
 				});
 				return Items;
 			});
@@ -35,9 +35,9 @@ const Sidebar = ({ isOpen }) => {
 		return () => {
 			setMainMenu(MENUITEMS);
 		};
-	}, [isChange]);
+	},[isChange]);
 
-	const setNavActive = (item) => {
+  const setNavActive = (item) => {
 		setIsChange(!isChange)
 		MENUITEMS.filter((menuItem) => {
 			if (menuItem !== item) menuItem.active = false;			
@@ -46,19 +46,19 @@ const Sidebar = ({ isOpen }) => {
 		item.active = !item.active;
 		setMainMenu(MENUITEMS);
 	};
-
+	
 	const mainMenu = mainmenu.map((menuItem, i) => (
-		<li className={!menuItem.active ? "option-sidebar" : "option-sidebar active-op"} key={i}>
+		<li className={!menuItem.active ?"option-sidebar":"option-sidebar active-op"} key={i}>							
 			<Link
 				to={`${process.env.PUBLIC_URL}${menuItem.path}`}
 				className={!menuItem.active ? "option-header" : "option-header active-op-hd"}
 				onClick={() => setNavActive(menuItem)}
 			>
-				<menuItem.icon className={!isOpen ? "option-header-icon" : "option-header-icon closed-ops"} />
-				<span>{menuItem.title}</span>
-			</Link>
+        		<menuItem.icon className={!isOpen ? "option-header-icon" : "option-header-icon closed-ops"}/>
+        		<span>{menuItem.title}</span>        
+      		</Link>						
 		</li>
-	));
+	));	
 
 	const mainMenuClosed = mainmenu.map((menuItem, i) => (
 		<li className={!menuItem.active ?"option-sidebar-cl":"option-sidebar-cl active-op"} key={i}>							
@@ -71,7 +71,7 @@ const Sidebar = ({ isOpen }) => {
       		</Link>						
 		</li>
 	));	
-
+	
 
 	return (
 		<Fragment>
@@ -79,8 +79,8 @@ const Sidebar = ({ isOpen }) => {
 				<div className={!isOpen ? "header-sidebar" : "header-sidebar closed-hd"}>
 					<div className="header-sidebar-logo">
 						<Link to={`${process.env.PUBLIC_URL}/inicio`}>
-							<img className={!isOpen ? "uta-logo" : "uta-logo closed-lg"} src={logo} alt="Inicio" />
-						</Link>
+							<img className={!isOpen ? "uta-logo" : "uta-logo closed-lg"} src={logo} alt="Inicio"/>
+						</Link>            
 					</div>
 					<div className={!isOpen ? "header-sidebar-title" : "header-sidebar-title closed-ti"}>
 						<span className="title-name">Universidad</span>
@@ -89,18 +89,26 @@ const Sidebar = ({ isOpen }) => {
 				</div>
 				<div className="body-sidebar">
 					{!isOpen ? (
-						<span className="menu-span">Menu</span>
+						<span className="menu-span">Menu</span>							
 					) : (
 						<div className="menu-icon-side">
-							<Box />
+							<Box/>
 						</div>
-					)}
-					<div className="separator" />
+					)}	
+					<div className="separator"/>
 					<div className="body-sidebar-options">
 						{!isOpen ? mainMenu : mainMenuClosed}
 						<li className={!isOpen ? "option-sidebar logout-sidebar" : "option-sidebar-cl logout-sidebar"}>
 							<Link to={`${process.env.PUBLIC_URL}/`} className="option-header">
-
+								{!isOpen ? (
+									<Fragment>
+										<LogOut className="option-header-icon"/>
+										<span>Cerrar Sesion</span>
+									</Fragment>
+								) : (
+									<LogOut className="option-header-icon"/>
+								)}
+								
 							</Link>
 						</li>
 					</div>
