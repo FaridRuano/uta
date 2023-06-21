20499253bffc30489328bdcf6c42fb6e72800722
Diego Cata <difercata26@gmail.com>
2023-06-20 22:15:39 -0500

    Sidebar Mejorado

--- src/components/common/sidebar/sidebar.jsx
+++ src/components/common/sidebar/sidebar.jsx
@@ -2,8 +2,10 @@ import React, { Fragment, useEffect, useState } from "react"
 import { MENUITEMS } from './menu'
 import { Link } from "react-router-dom"
 import  logo from "../../../assets/images/logo-uta.png"
-import { LogOut } from "react-feather";
-const Sidebar = () => {
+import { LogOut, Box } from "react-feather";
+import "./style.scss"
+
+const Sidebar = ({ isOpen }) => {
   const [mainmenu, setMainMenu] = useState(MENUITEMS)
 	const [isChange, setIsChange] = useState(false)
 
@@ -17,12 +19,6 @@ const Sidebar = () => {
 					if (subItems.path === currentUrl) setNavActive(subItems);
 					if (!subItems.children) return false;
 					subItems.children.filter((subSubItems) => {
-						if (subSubItems.path === currentUrl) {
-							setNavActive(subSubItems);
-							return true;
-						} else {
-							return false;
-						}
 					});
 					return subItems;
 				});
@@ -34,51 +30,52 @@ const Sidebar = () => {
 			setMainMenu(MENUITEMS);
 		};
 	},[isChange]);
-
-  const setNavActive = (item) => {
-		setIsChange(!isChange)
-		MENUITEMS.filter((menuItem) => {
-			if (menuItem !== item) menuItem.active = false;			
-			return false;
-		});
-		item.active = !item.active;
-		setMainMenu(MENUITEMS);
-	};
 	
 	const mainMenu = mainmenu.map((menuItem, i) => (
-		<li className="active" key={i}>							
-      <Link
-        to={`${process.env.PUBLIC_URL}${menuItem.path}`}
-        className="sidebar-header active"
-        onClick={() => setNavActive(menuItem)}
-      >
-        <menuItem.icon />
-        <span>{menuItem.title}</span>        
-      </Link>						
+		<li className={!menuItem.active ?"option-sidebar":"option-sidebar active-op"} key={i}>							
+			<Link
+				to={`${process.env.PUBLIC_URL}${menuItem.path}`}
+				className={!menuItem.active ? "option-header" : "option-header active-op-hd"}
+				onClick={() => setNavActive(menuItem)}
+			>
+        		<menuItem.icon className={!isOpen ? "option-header-icon" : "option-header-icon closed-ops"}/>
+        		<span>{menuItem.title}</span>        
+      		</Link>						
 		</li>
-	));
+	));		
 	
 
 	return (
 		<Fragment>
-			<div className="page-sidebar">
-				<div className="main-header-left d-none d-lg-block">
-					<div className="logo-wrapper">
+			<div className={!isOpen ? "page-sidebar" : "page-sidebar closed"}>
+				<div className={!isOpen ? "header-sidebar" : "header-sidebar closed-hd"}>
+					<div className="header-sidebar-logo">
 						<Link to={`${process.env.PUBLIC_URL}/inicio`}>
-							<img className="blur-up lazyloaded" src={logo} alt="" style={{height: '66px'}}/>
+							<img className={!isOpen ? "uta-logo" : "uta-logo closed-lg"} src={logo} alt="Inicio"/>
 						</Link>            
 					</div>
+					<div className={!isOpen ? "header-sidebar-title" : "header-sidebar-title closed-ti"}>
+						<span className="title-name">Universidad</span>
+						<span className="title-subname">Tecnica de Ambato</span>
+					</div>
 				</div>
-				<div className="sidebar custom-scrollbar">
-					<ul className="sidebar-menu">
-            {mainMenu}
-            <li>
-              <Link to={`${process.env.PUBLIC_URL}/`}>
-                <LogOut/>
-                <span>Cerrar Sesion</span>
-              </Link>
-            </li>
-          </ul>
+				<div className="body-sidebar">
+					{!isOpen ? (
+						<span className="menu-span">Menu</span>							
+					) : (
+						<div className="menu-icon-side">
+							<Box/>
+						</div>
+					)}	
+					<div className="separator"/>
+					<div className="body-sidebar-options">
+						{!isOpen ? mainMenu : mainMenuClosed}
+						<li className={!isOpen ? "option-sidebar logout-sidebar" : "option-sidebar-cl logout-sidebar"}>
+							<Link to={`${process.env.PUBLIC_URL}/`} className="option-header">
+								
+							</Link>
+						</li>
+					</div>
 				</div>
 			</div>
 		</Fragment>
