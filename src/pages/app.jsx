e3120f5209b53d04540f86b95e8f187ed90b0c10
LennyMA <lmoreno3918@uta.edu.ec>
2023-07-09 00:58:41 -0500

    archivo app.jsx corregido

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,29 +1,30 @@
-import { Outlet } from "react-router-dom"
-import Sidebar from "../components/common/sidebar/sidebar"
-import '../assets/scss/app_page.scss'
-import { Fragment, useState } from "react"
-import Header from "../components/common/header/header"
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/common/sidebar/sidebar";
+import "../assets/scss/app_page.scss";
+import React, { useState } from "react";
+import Header from "../components/common/header/header";
+
+export const Context = React.createContext();
 
 const App = () => {
-	const [sideOpen, setSideOpen] = useState(true)
+  const [sideOpen, setSideOpen] = useState(true);
 
-	const toggleOpen = () => {
-		setSideOpen(!sideOpen)
-		console.log(sideOpen)
-	}
+  const toggleOpen = () => {
+    setSideOpen(!sideOpen);
+  };
 
-	return (
-		<div className="main-page">
-			<div className="page-container">
-				<Sidebar className="sidebar-page" isOpen={sideOpen}/>
-				<Header activeNav={toggleOpen} isOpen={sideOpen}/>	
-				<div className={!sideOpen ? "main-content" : "main-content-cl"}>
-				<div className={!sideOpen ? "main-content" : "main-content-cl"}>
-					<Header className="header-page" activeNav={toggleOpen}/>	
-					<Outlet/>
-				</div>
-			</div>
-		</div>
-	)
-}
-export default App
+  return (
+    <Context.Provider value={[sideOpen, setSideOpen]}>
+      <div className="main-page">
+        <div className="page-container">
+          <Sidebar className="sidebar-page" isOpen={sideOpen} />
+          <Header activeNav={toggleOpen} isOpen={sideOpen} />
+          <div className={!sideOpen ? "main-content" : "main-content-cl"}>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </Context.Provider>
+  );
+};
+export default App;
